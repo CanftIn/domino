d04f878e4f40eb820a01937f49182adc6a9562e4
CanftIn <wwc7033@gmail.com>
2023-04-25 03:20:56 +0000

    update STLExtras

--- include/domino/util/STLExtras.h
+++ include/domino/util/STLExtras.h
@@ -337,6 +337,26 @@ auto map_range(ContainerTy &&C, FuncTy F) {
 template <typename T, typename... Ts>
 using is_one_of = std::disjunction<std::is_same<T, Ts>...>;
 
+namespace detail {
+template <class, template <class...> class Op, class... Args> struct detector {
+  using value_t = std::false_type;
+};
+template <template <class...> class Op, class... Args>
+struct detector<std::void_t<Op<Args...>>, Op, Args...> {
+  using value_t = std::true_type;
+};
+} // end namespace detail
+
+/// Detects if a given trait holds for some set of arguments 'Args'.
+/// For example, the given trait could be used to detect if a given type
+/// has a copy assignment operator:
+///   template<class T>
+///   using has_copy_assign_t = decltype(std::declval<T&>()
+///                                                 = std::declval<const T&>());
+///   bool fooHasCopyAssign = is_detected<has_copy_assign_t, FooClass>::value;
+template <template <class...> class Op, class... Args>
+using is_detected = typename detail::detector<void, Op, Args...>::value_t;
+
 }  // namespace domino
 
 #endif  // DOMINO_UTIL_STLEXTRAS_H_
