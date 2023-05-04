a4d068aa74ba1a796c916a2798bddce49a63d7af
CanftIn <wwc7033@gmail.com>
2023-05-04 07:27:30 +0000

    add script mlir codegen test

---
+++ include/domino/script/Dialect.h
@@ -0,0 +1,12 @@
+#ifndef DOMINO_SCRIPT_DIALECT_H_
+#define DOMINO_SCRIPT_DIALECT_H_
+
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/FunctionInterfaces.h"
+#include "mlir/IR/SymbolTable.h"
+#include "mlir/Interfaces/CallInterfaces.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
+
+
+
+#endif  // DOMINO_SCRIPT_DIALECT_H_
---
+++ unittests/script/MLIRCodegen.t.cc
@@ -0,0 +1,6 @@
+#include <domino/script/Dialect.h>
+
+int main() {
+  
+  return 0;
+}
