7cc6a4ecc9d4c54093cdc09e0436eb512254e160
CanftIn <wwc7033@gmail.com>
2023-06-21 17:39:42 +0800

    update any util, be more compatible with std::any

--- include/domino/util/Any.h
+++ include/domino/util/Any.h
@@ -9,132 +9,225 @@
 
 namespace domino {
 
-class Any {
+class any {
   template <typename T>
   struct TypeId {
-    static char Id;
+    static char id;
   };
+
+  template <typename T>
+  struct IsInPlaceType;
 
   struct StorageBase {
     virtual ~StorageBase() = default;
-    virtual std::unique_ptr<StorageBase> clone() const = 0;
-    virtual const void *id() const = 0;
+    virtual auto clone() const -> std::unique_ptr<StorageBase> = 0;
+    virtual auto id() const noexcept -> const void* = 0;
   };
 
   template <typename T>
   struct StorageImpl : public StorageBase {
-    explicit StorageImpl(const T &Value) : Value(Value) {}
+    template <typename... Args>
+    explicit StorageImpl(std::in_place_t /*unused*/, Args&&... args)
+        : value(std::forward<Args>(args)...) {}
 
-    explicit StorageImpl(T &&Value) : Value(std::move(Value)) {}
-
-    std::unique_ptr<StorageBase> clone() const override {
-      return std::make_unique<StorageImpl<T>>(Value);
+    auto clone() const -> std::unique_ptr<StorageBase> final {
+      return std::unique_ptr<StorageBase>(
+          new StorageImpl(std::in_place, value));
     }
 
-    const void *id() const override { return &TypeId<T>::Id; }
+    auto id() const noexcept -> const void* final { return &TypeId<T>::id; }
 
-    T Value;
+    T value;
 
-   private:
-    StorageImpl &operator=(const StorageImpl &Other) = delete;
-    StorageImpl(const StorageImpl &Other) = delete;
+    auto operator=(const StorageImpl& other) -> StorageImpl& = delete;
+    StorageImpl(const StorageImpl& other) = delete;
   };
 
  public:
-  Any() = default;
+  constexpr any() noexcept = default;
 
-  Any(const Any &Other)
-      : Storage(Other.Storage ? Other.Storage->clone() : nullptr) {}
+  any(const any& other)
+      : obj_(other.has_value() ? other.obj_->clone()
+                               : std::unique_ptr<StorageBase>()) {}
 
-  template <typename T,
+  template <typename T, typename VT = std::decay_t<T>,
             std::enable_if_t<
-                std::conjunction_v<
-                    std::negation<std::is_same<std::decay_t<T>, Any>>,
-                    std::negation<std::is_convertible<Any, std::decay_t<T>>>,
-                    std::is_copy_constructible<std::decay_t<T>>>,
+                std::conjunction_v<std::negation<std::is_same<VT, any>>,
+                                   std::negation<IsInPlaceType<VT>>,
+                                   std::negation<std::is_convertible<any, VT>>,
+                                   std::is_copy_constructible<VT>>,
                 int> = 0>
-  Any(T &&Value) {
-    Storage =
-        std::make_unique<StorageImpl<std::decay_t<T>>>(std::forward<T>(Value));
-  }
+  any(T&& value)
+      : obj_(new StorageImpl<VT>(std::in_place, std::forward<T>(value))) {}
 
-  Any(Any &&Other) : Storage(std::move(Other.Storage)) {}
+  template <
+      typename T, typename... Args, typename VT = std::decay_t<T>,
+      std::enable_if_t<std::conjunction_v<std::is_copy_constructible<VT>,
+                                          std::is_constructible<VT, Args...>>,
+                       int> = 0>
+  explicit any(std::in_place_type_t<T> /*unused*/, Args&&... args)
+      : obj_(new StorageImpl<VT>(std::in_place, std::forward<Args>(args)...)) {}
 
-  Any &swap(Any &Other) {
-    std::swap(Storage, Other.Storage);
+  template <
+      typename T, typename U, typename... Args, typename VT = std::decay_t<T>,
+      std::enable_if_t<
+          std::conjunction_v<
+              std::is_copy_constructible<VT>,
+              std::is_constructible<VT, std::initializer_list<U>&, Args...>>,
+          int> = 0>
+  explicit any(std::in_place_type_t<T> /*unused*/,
+               std::initializer_list<U> ilist, Args&&... args)
+      : obj_(new StorageImpl<VT>(std::in_place, ilist,
+                                 std::forward<Args>(args)...)) {}
+
+  any(any&& other) noexcept = default;
+
+  auto operator=(const any& other) -> any& {
+    any(other).swap(*this);
     return *this;
   }
 
-  Any &operator=(Any Other) {
-    Storage = std::move(Other.Storage);
+  auto operator=(any&& other) noexcept -> any& {
+    any(std::move(other)).swap(*this);
     return *this;
   }
 
-  bool has_value() const { return !!Storage; }
+  template <typename T, typename VT = std::decay_t<T>,
+            std::enable_if_t<
+                std::conjunction_v<std::negation<std::is_same<VT, any>>,
+                                   std::negation<IsInPlaceType<VT>>,
+                                   std::negation<std::is_convertible<any, VT>>,
+                                   std::is_copy_constructible<VT>>,
+                int> = 0>
+  auto operator=(T&& value) -> any& {
+    any(std::in_place_type_t<VT>(), std::forward<T>(value)).swap(*this);
+    return *this;
+  }
 
-  void reset() { Storage.reset(); }
+  template <typename T, typename... Args, typename VT = std::decay_t<T>,
+            std::enable_if_t<std::is_copy_constructible_v<VT> &&
+                                 std::is_constructible_v<VT, Args...>,
+                             int> = 0>
+  auto emplace(Args&&... args) -> VT& {
+    reset();
+    auto* const object_ptr =
+        new StorageImpl<VT>(std::in_place, std::forward<Args>(args)...);
+    obj_ = std::unique_ptr<StorageBase>(object_ptr);
+    return object_ptr->value;
+  }
+
+  template <
+      typename T, typename U, typename... Args, typename VT = std::decay_t<T>,
+      std::enable_if_t<
+          std::is_copy_constructible_v<VT> &&
+              std::is_constructible_v<VT, std::initializer_list<U>&, Args...>,
+          int> = 0>
+  auto emplace(std::initializer_list<U> ilist, Args&&... args) -> VT& {
+    reset();
+    auto* const object_ptr =
+        new StorageImpl<VT>(std::in_place, ilist, std::forward<Args>(args)...);
+    obj_ = std::unique_ptr<StorageBase>(object_ptr);
+    return object_ptr->value;
+  }
+
+  auto has_value() const noexcept -> bool { return obj_ != nullptr; }
+
+  auto reset() noexcept -> void { obj_.reset(); }
+
+  auto swap(any& other) noexcept -> void { obj_.swap(other.obj_); }
 
  private:
   template <typename T>
-  bool isa() const {
-    if (!Storage) return false;
-    return Storage->id() == &Any::TypeId<remove_cvref_t<T>>::Id;
+  auto isa() const -> bool {
+    if (!obj_) {
+      return false;
+    }
+    return obj_->id() == &any::TypeId<remove_cvref_t<T>>::id;
+  }
+
+  auto CloneObj() const -> std::unique_ptr<StorageBase> {
+    if (!obj_) {
+      return nullptr;
+    }
+    return obj_->clone();
   }
 
   template <class T>
-  friend T any_cast(const Any &Value);
+  friend auto any_cast(const any& value) -> T;
   template <class T>
-  friend T any_cast(Any &Value);
+  friend auto any_cast(any& value) -> T;
   template <class T>
-  friend T any_cast(Any &&Value);
+  friend auto any_cast(any&& value) -> T;
   template <class T>
-  friend const T *any_cast(const Any *Value);
+  friend auto any_cast(const any* value) noexcept -> const T*;
   template <class T>
-  friend T *any_cast(Any *Value);
+  friend auto any_cast(any* value) noexcept -> T*;
   template <typename T>
-  friend bool any_isa(const Any &Value);
+  friend auto any_isa(const any& value) -> bool;
 
-  std::unique_ptr<StorageBase> Storage;
+  std::unique_ptr<StorageBase> obj_;
 };
 
 template <typename T>
-char Any::TypeId<T>::Id = 0;
+char any::TypeId<T>::id = 0;
 
 template <typename T>
-bool any_isa(const Any &Value) {
-  return Value.isa<T>();
+struct any::IsInPlaceType : std::false_type {};
+
+template <typename T>
+struct any::IsInPlaceType<std::in_place_type_t<T>> : std::true_type {};
+
+inline void swap(any& x, any& y) noexcept { x.swap(y); }
+
+template <typename T, typename... Args>
+auto make_any(Args&&... args) -> any {
+  return any(std::in_place_type_t<T>(), std::forward<Args>(args)...);
+}
+
+template <typename T, typename U, typename... Args>
+auto make_any(std::initializer_list<U> il, Args&&... args) -> any {
+  return any(std::in_place_type_t<T>(), il, std::forward<Args>(args)...);
+}
+
+template <typename T>
+auto any_isa(const any& value) -> bool {
+  return value.isa<T>();
 }
 
 template <class T>
-T any_cast(const Any &Value) {
-  assert(Value.isa<T>() && "Bad any cast!");
-  return static_cast<T>(*any_cast<remove_cvref_t<T>>(&Value));
+auto any_cast(const any& value) -> T {
+  assert(value.isa<T>() && "Bad any cast!");
+  return static_cast<T>(*any_cast<remove_cvref_t<T>>(&value));
 }
 
 template <class T>
-T any_cast(Any &Value) {
-  assert(Value.isa<T>() && "Bad any cast!");
-  return static_cast<T>(*any_cast<remove_cvref_t<T>>(&Value));
+auto any_cast(any& value) -> T {
+  assert(value.isa<T>() && "Bad any cast!");
+  return static_cast<T>(*any_cast<remove_cvref_t<T>>(&value));
 }
 
 template <class T>
-T any_cast(Any &&Value) {
-  assert(Value.isa<T>() && "Bad any cast!");
-  return static_cast<T>(std::move(*any_cast<remove_cvref_t<T>>(&Value)));
+auto any_cast(any&& value) -> T {
+  assert(value.isa<T>() && "Bad any cast!");
+  return static_cast<T>(std::move(*any_cast<remove_cvref_t<T>>(&value)));
 }
 
 template <class T>
-const T *any_cast(const Any *Value) {
+auto any_cast(const any* value) noexcept -> const T* {
   using U = remove_cvref_t<T>;
-  if (!Value || !Value->isa<U>()) return nullptr;
-  return &static_cast<Any::StorageImpl<U> &>(*Value->Storage).Value;
+  if (!value || !value->isa<U>()) {
+    return nullptr;
+  }
+  return &static_cast<any::StorageImpl<U>&>(*value->obj_).value;
 }
 
 template <class T>
-T *any_cast(Any *Value) {
+auto any_cast(any* value) noexcept -> T* {
   using U = std::decay_t<T>;
-  if (!Value || !Value->isa<U>()) return nullptr;
-  return &static_cast<Any::StorageImpl<U> &>(*Value->Storage).Value;
+  if (!value || !value->isa<U>()) {
+    return nullptr;
+  }
+  return &static_cast<any::StorageImpl<U>&>(*value->obj_).value;
 }
 
 }  // namespace domino
--- unittests/util/Any.t.cc
+++ unittests/util/Any.t.cc
@@ -3,16 +3,17 @@
 
 #include <cstdlib>
 
-using namespace domino;
+namespace domino {
+namespace {
 
 TEST(AnyTest, ConstructionAndAssignment) {
-  Any A;
-  Any B{7};
-  Any C{8};
-  Any D{"hello"};
-  Any E{3.7};
+  any A;
+  any B{7};
+  any C{8};
+  any D{"hello"};
+  any E{3.7};
 
-  // An empty Any is not anything.
+  // An empty any is not anything.
   EXPECT_FALSE(A.has_value());
   EXPECT_FALSE(any_cast<int>(&A));
 
@@ -26,7 +27,7 @@ TEST(AnyTest, ConstructionAndAssignment) {
 
   // A const char * is a const char * but not an int.
   EXPECT_TRUE(D.has_value());
-  EXPECT_TRUE(any_cast<const char *>(&D));
+  EXPECT_TRUE(any_cast<const char*>(&D));
   EXPECT_FALSE(any_cast<int>(&D));
 
   // A double is a double but not a float.
@@ -36,7 +37,7 @@ TEST(AnyTest, ConstructionAndAssignment) {
 
   // After copy constructing from an int, the new item and old item are both
   // ints.
-  Any F(B);
+  any F(B);
   EXPECT_TRUE(B.has_value());
   EXPECT_TRUE(F.has_value());
   EXPECT_TRUE(any_cast<int>(&F));
@@ -44,7 +45,7 @@ TEST(AnyTest, ConstructionAndAssignment) {
 
   // After move constructing from an int, the new item is an int and the old one
   // isn't.
-  Any G(std::move(C));
+  any G(std::move(C));
   EXPECT_FALSE(C.has_value());
   EXPECT_TRUE(G.has_value());
   EXPECT_TRUE(any_cast<int>(&G));
@@ -66,27 +67,27 @@ TEST(AnyTest, ConstructionAndAssignment) {
 }
 
 TEST(AnyTest, GoodAnyCast) {
-  Any A;
-  Any B{7};
-  Any C{8};
-  Any D{"hello"};
-  Any E{'x'};
+  any A;
+  any B{7};
+  any C{8};
+  any D{"hello"};
+  any E{'x'};
 
   // Check each value twice to make sure it isn't damaged by the cast.
   EXPECT_EQ(7, any_cast<int>(B));
   EXPECT_EQ(7, any_cast<int>(B));
 
-  EXPECT_STREQ("hello", any_cast<const char *>(D));
-  EXPECT_STREQ("hello", any_cast<const char *>(D));
+  EXPECT_STREQ("hello", any_cast<const char*>(D));
+  EXPECT_STREQ("hello", any_cast<const char*>(D));
 
   EXPECT_EQ('x', any_cast<char>(E));
   EXPECT_EQ('x', any_cast<char>(E));
 
-  Any F(B);
+  any F(B);
   EXPECT_EQ(7, any_cast<int>(F));
   EXPECT_EQ(7, any_cast<int>(F));
 
-  Any G(std::move(C));
+  any G(std::move(C));
   EXPECT_EQ(8, any_cast<int>(G));
   EXPECT_EQ(8, any_cast<int>(G));
 
@@ -106,33 +107,33 @@ TEST(AnyTest, GoodAnyCast) {
   // Make sure moving from pointers gives back pointers, and that we can modify
   // the underlying value through those pointers.
   EXPECT_EQ(7, *any_cast<int>(&A));
-  int *N = any_cast<int>(&A);
+  int* N = any_cast<int>(&A);
   *N = 42;
   EXPECT_EQ(42, any_cast<int>(A));
 
   // Make sure that we can any_cast to a reference and this is considered a good
   // cast, resulting in an lvalue which can be modified.
-  any_cast<int &>(A) = 43;
+  any_cast<int&>(A) = 43;
   EXPECT_EQ(43, any_cast<int>(A));
 }
 
 TEST(AnyTest, CopiesAndMoves) {
   struct TestType {
     TestType() = default;
-    TestType(const TestType &Other)
+    TestType(const TestType& Other)
         : Copies(Other.Copies + 1), Moves(Other.Moves) {}
-    TestType(TestType &&Other) : Copies(Other.Copies), Moves(Other.Moves + 1) {}
+    TestType(TestType&& Other) : Copies(Other.Copies), Moves(Other.Moves + 1) {}
     int Copies = 0;
     int Moves = 0;
   };
 
   // One move to get TestType into the Any, and one move on the cast.
-  TestType T1 = any_cast<TestType>(Any{TestType()});
+  TestType T1 = any_cast<TestType>(any{TestType()});
   EXPECT_EQ(0, T1.Copies);
   EXPECT_EQ(2, T1.Moves);
 
   // One move to get TestType into the Any, and one copy on the cast.
-  Any A{TestType()};
+  any A{TestType()};
   TestType T2 = any_cast<TestType>(A);
   EXPECT_EQ(1, T2.Copies);
   EXPECT_EQ(1, T2.Moves);
@@ -144,19 +145,22 @@ TEST(AnyTest, CopiesAndMoves) {
 }
 
 TEST(AnyTest, BadAnyCast) {
-  Any A;
-  Any B{7};
-  Any C{"hello"};
-  Any D{'x'};
+  any A;
+  any B{7};
+  any C{"hello"};
+  any D{'x'};
 
 #if !defined(NDEBUG) && GTEST_HAS_DEATH_TEST
   EXPECT_DEATH(any_cast<int>(A), "");
 
   EXPECT_DEATH(any_cast<float>(B), "");
-  EXPECT_DEATH(any_cast<int *>(B), "");
+  EXPECT_DEATH(any_cast<int*>(B), "");
 
   EXPECT_DEATH(any_cast<std::string>(C), "");
 
   EXPECT_DEATH(any_cast<unsigned char>(D), "");
 #endif
 }
+
+}  // namespace
+}  // namespace domino
